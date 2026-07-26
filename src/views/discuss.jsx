fbce97d6d5cba31e1ada95a48109712a79b546be
Roman Kudryavtsev <kudryavtsevroman@mail.ru>
2026-07-26 22:39:29 +0300

    fix discuss desktop page

--- src/views/discuss.jsx
+++ src/views/discuss.jsx
@@ -4,7 +4,7 @@ import { $t } from "@/utils/lang.utils";
 
 export default function DiscussPage({ lang }) {
     return (
-        <div className="bg-soft-surface lg:discuss-bg">
+        <div className="bg-soft-surface lg:min-h-dvh lg:bg-[url('/discuss-bg.png')] lg:bg-center lg:bg-cover lg:bg-no-repeat">
             <section className="flex flex-col gap-6 lg:gap-7.5 lg:items-center">
                 <div className="flex flex-col gap-2.5 lg:gap-3.5 max-w-4xl w-full">
                     <h2 className="lg:text-center text-4xl lg:text-5xl text-primary font-semibold">{ $t('project_form.title', lang) }</h2>
